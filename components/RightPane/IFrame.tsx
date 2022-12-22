dad2a3441036aa883cec894af2cc62e53cc2ee3a
Subho <contact.subhoghosh@gmail.com>
2022-12-22 18:58:51 +0530

    fix: iframe flickering

--- components/RightPane/IFrame.tsx
+++ components/RightPane/IFrame.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { useDocStore } from "../../store/doc";
 
 interface Props {
@@ -9,12 +10,28 @@ export default function IFrame({ twEnabled }: Props) {
   const css = useDocStore((state) => state.css);
   const js = useDocStore((state) => state.js);
 
-  return (
-    <iframe
-      className="h-full w-full"
-      srcDoc={`<style>${css}</style>${html}${
-        twEnabled ? '<script src="https://cdn.tailwindcss.com"></script>' : ""
-      }<script>${js}</script>`}
-    ></iframe>
-  );
+  const iframeRef = useRef<null | HTMLIFrameElement>(null);
+
+  useEffect(() => {
+    if (iframeRef.current?.contentDocument) {
+      iframeRef.current.contentDocument.body.innerHTML = `${html}<style>${css}</style>`;
+      const head = iframeRef.current.contentDocument.querySelector("head");
+      const myscript = document.createElement("script");
+      myscript.type = "text/javascript";
+      myscript.innerText = js;
+      head?.appendChild(myscript);
+    }
+  }, [html, css, js]);
+
+  useEffect(() => {
+    if (iframeRef.current?.contentDocument) {
+      if (twEnabled) {
+        iframeRef.current.srcdoc = `<style>${css}</style>${html}<script src='https://cdn.tailwindcss.com'></script><script>${js}</script>`;
+      } else {
+        iframeRef.current.srcdoc = `<style>${css}</style>${html}<script>${js}</script>`;
+      }
+    }
+  }, [twEnabled]);
+
+  return <iframe className="h-full w-full" ref={iframeRef}></iframe>;
 }
