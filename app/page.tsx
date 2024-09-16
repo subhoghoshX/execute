2be4b5ea6bc6bf19ada58c5aa4c09714e071b67e
Subho <contact.subhoghosh@gmail.com>
2024-09-16 21:13:57 +0530

    save
    prepare to move to vite

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,252 @@
-import Image from "next/image";
+"use client";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Button } from "@/components/ui/button"
+import ReactCodeMirror from "@uiw/react-codemirror";
+import { useEffect, useState } from "react";
+import { MonitorCog, Moon, Settings, Sun } from "lucide-react";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { html } from "@codemirror/lang-html"
+import { css } from "@codemirror/lang-css";
+import { javascript } from "@codemirror/lang-javascript";
+import { vim } from "@replit/codemirror-vim";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [htmlBuffer, setHtmlBuffer] = useState("");
+  const [cssBuffer, setCssBuffer] = useState("");
+  const [jsBuffer, setJsBuffer] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const [htmlCode, setHtmlCode] = useState("");
+  const [cssCode, setCssCode] = useState("");
+  const [jsCode, setJsCode] = useState("");
+
+  const isHtmlDirty = htmlBuffer !== htmlCode;
+  const isCssDirty = cssBuffer !== cssCode;
+  const isJsDirty = jsBuffer !== jsCode;
+
+  const [activeTab, setActiveTab] = useState("html");
+
+  const [isVimEnabled, setIsVimEnabled] = useState(false);
+  useEffect(() => {
+    const value = window.localStorage.getItem("isVimEnabled");
+    if (value === "true"){
+      setIsVimEnabled(true);
+    } else {
+      setIsVimEnabled(false);
+    }
+  }, []);
+
+  const [isTailwindEnalbed, setIsTailwindEnabled] = useState(false);
+  useEffect(() => {
+    const value = window.localStorage.getItem("isTailwindEnabled") ?? false;
+    if (value === "true") {
+      setIsTailwindEnabled(true);
+    } else {
+      setIsTailwindEnabled(false);
+    }
+  }, []);
+
+  const srcDoc = generateSourceDoc(htmlCode, cssCode, jsCode, isTailwindEnalbed);
+
+  return (
+    <main className="h-screen">
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel defaultSize={50}>
+          <Tabs defaultValue="html" value={activeTab} onValueChange={setActiveTab} className="flex flex-col h-full overflow-hidden">
+            <menu className="p-2 border-b border-zinc-200 flex items-center gap-2">
+              <TabsList>
+                <TabsTrigger value="html" className="relative">
+                  HTML
+                  {isHtmlDirty && (
+                    <span className="size-1 rounded-full absolute bg-blue-500 top-1 right-1"></span>
+                  )}
+                </TabsTrigger>
+                <TabsTrigger value="css" className="relative">
+                  CSS
+                  {isCssDirty && (
+                    <span className="size-1 rounded-full absolute bg-blue-500 top-1 right-1"></span>
+                  )}
+                </TabsTrigger>
+                <TabsTrigger value="js" className="relative">
+                  JS
+                  {isJsDirty && (
+                    <span className="size-1 rounded-full absolute bg-blue-500 top-1 right-1"></span>
+                  )}
+                </TabsTrigger>
+              </TabsList>
+
+              <Popover>
+                <PopoverTrigger className="ml-auto" asChild>
+                  <Button size="icon" variant="outline">
+                    <Settings className="size-4" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="flex flex-col gap-3">
+                  <div className="flex items-center gap-2">
+                    <Switch id="vim-mode" checked={isVimEnabled} onCheckedChange={(isChecked) => {
+                      setIsVimEnabled(isChecked);
+                      localStorage.setItem("isVimEnabled", isChecked.toString());
+                    }
+                    } />
+                    <Label htmlFor="vim-mode">Vim</Label>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <Switch id="tailwind" checked={isTailwindEnalbed} onCheckedChange={(isChecked) => {
+                      setIsTailwindEnabled(isChecked);
+                      localStorage.setItem("isTailwindEnabled", isChecked.toString());
+                    }
+                    } />
+                    <Label htmlFor="tailwind">TailwindCSS</Label>
+                  </div>
+                  <ToggleGroup type="single" size="sm" defaultValue="system" className="justify-start" variant="outline">
+                    <ToggleGroupItem value="system">
+                      <MonitorCog className="size-4 mr-2" />
+                      System
+                    </ToggleGroupItem>
+                    <ToggleGroupItem value="light">
+                      <Sun className="size-4 mr-2" />
+                      Light
+                    </ToggleGroupItem>
+                    <ToggleGroupItem value="dark">
+                      <Moon className="size-4 mr-2" />
+                      Dark
+                    </ToggleGroupItem>
+                  </ToggleGroup>
+                </PopoverContent>
+              </Popover>
+              <Button>Share</Button>
+            </menu>
+            <TabsContent value="html" className="mt-0 flex-grow overflow-hidden">
+              <ReactCodeMirror
+                className="h-full [&>.cm-editor]:outline-none [&>.cm-editor]:h-full"
+                value={htmlBuffer}
+                onChange={(value) => setHtmlBuffer(value)}
+                onKeyDown={(e) => {
+                  if ((e.ctrlKey || e.metaKey) && (e.key === 's' || e.key === 'S')) {
+                    e.preventDefault();
+                    setHtmlCode(htmlBuffer);
+                  }
+                  if (e.altKey && e.key === '1') {
+                    e.preventDefault();
+                  }
+                  if (e.altKey && e.key === '2') {
+                    e.preventDefault();
+                    setActiveTab("css");
+                  }
+                  if (e.altKey && e.key === '3') {
+                    e.preventDefault();
+                    setActiveTab("js");
+                  }
+                }}
+                onCreateEditor={(view) => {
+                  view.focus();
+                }}
+                extensions={[...(isVimEnabled ? [vim()] : []), html()]}
+              />
+            </TabsContent>
+            <TabsContent value="css" className="mt-0 flex-grow">
+              <ReactCodeMirror
+                className="h-full [&>.cm-editor]:outline-none [&>.cm-editor]:h-full"
+                value={cssBuffer}
+                onChange={(value) => setCssBuffer(value)}
+                onKeyDown={(e) => {
+                  if ((e.ctrlKey || e.metaKey) && (e.key === 's' || e.key === 'S')) {
+                    e.preventDefault();
+                    setCssCode(cssBuffer);
+                  }
+                  if (e.altKey && e.key === '1') {
+                    e.preventDefault();
+                    setActiveTab("html");
+                  }
+                  if (e.altKey && e.key === '2') {
+                    e.preventDefault();
+                  }
+                  if (e.altKey && e.key === '3') {
+                    e.preventDefault();
+                    setActiveTab("js");
+                  }
+                }}
+                onCreateEditor={(view) => {
+                  view.focus();
+                }}
+                extensions={[...(isVimEnabled ? [vim()] : []), css()]}
+              />
+            </TabsContent>
+            <TabsContent value="js" className="mt-0 flex-grow">
+              <ReactCodeMirror
+                className="h-full [&>.cm-editor]:outline-none [&>.cm-editor]:h-full"
+                value={jsBuffer}
+                onChange={(value) => setJsBuffer(value)}
+                onKeyDown={(e) => {
+                  if ((e.ctrlKey || e.metaKey) && (e.key === 's' || e.key === 'S')) {
+                    e.preventDefault();
+                    setJsCode(jsBuffer);
+                  }
+                  if (e.altKey && e.key === '1') {
+                    e.preventDefault();
+                    setActiveTab("html");
+                  }
+                  if (e.altKey && e.key === '2') {
+                    e.preventDefault();
+                    setActiveTab("css");
+                  }
+                  if (e.altKey && e.key === '3') {
+                    e.preventDefault();
+                  }
+                }}
+                onCreateEditor={(view) => {
+                  view.focus();
+                }}
+                extensions={[...(isVimEnabled ? [vim()] : []), javascript()]}
+              />
+            </TabsContent>
+          </Tabs>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={50}>
+          <iframe className="h-full w-full" srcDoc={srcDoc}></iframe>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </main>
   );
+}
+
+function generateSourceDoc(html: string, css: string, js: string, isTailwindEnabled: boolean) {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+      <style>
+        ${css}
+      </style>
+      ${isTailwindEnabled ? '<script src="https://cdn.tailwindcss.com"></script>' : ''}
+    </head>
+    <body>
+      ${html}
+      <script type="text/javascript">
+        ${js}
+      </script>
+    </body>
+    </html>
+  `
 }
